262d73c1732823180f37e3c6fe1e53b8fe0a01b5
diego <diego@cvi047203.ugr.es>
2015-10-27 14:03:31 +0100

    Añadido los hondos

--- NapakalakiRuby/lib/prueba_napakalaki.rb
+++ NapakalakiRuby/lib/prueba_napakalaki.rb
@@ -9,23 +9,8 @@ require_relative "treasure_kind.rb"
 
 monsters = Array.new
 
-premio = Prize.new(1031, 7)
-mal_rollo1 = BadConsequence.newDeath("Se conecta. Estas muerto.")
-halconHodgson = Monster.new("Halcon cuco del Sr. Hodgson", 1031, mal_rollo1, premio)
-
-premio2 = Prize.new(0, 0)
-mal_rollo2 = BadConsequence.newLevelNumberOfTreasures("Eeeeeehhhhhh... Pierdes muchas horas de clase.", 9, 5, 4)
-sevilla = Monster.new("Sevilla", 9001, mal_rollo2, premio2)
-
-premio3 = Prize.new("a-bx=m", "\n\t(10)\n\t(2 )")
-mal_rollo3 = BadConsequence.newLevelSpecificTreasures("Te asusta, pero de buen rollo. Pierdes la cabeza.", 0, [TreasureKind::HELMET], nil)
-alem = Monster.new("Algebra lineal y estructuras matematicas", 19, mal_rollo3, premio3)
-
-monsters.push(halconHodgson)
-
-puts monsters[0]
-#puts halconHodgson
-#puts "\n"
-#puts sevilla
-#puts "\n"
-#puts alem
+# Los hondos
+bc_hondos = BadConsequence.newDeath("Estos monstruos resultan bastante superficiales y te aburren mortalmente. Estás muerto.")
+p_hondos = Prize.new(2, 1)
+los_hondos = Monster.new("Los hondos", 8, bc_hondos, p_hondos)
+monsters.push(los_hondos)
