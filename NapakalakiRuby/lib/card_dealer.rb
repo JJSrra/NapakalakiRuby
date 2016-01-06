ffd7cae553438febcfd49070ef2486b9f6975d39
Diego Iáñez <diegodan1893@gmail.com>
2016-01-06 13:33:45 +0100

    Especificadas las malas consecuencias

--- NapakalakiRuby/lib/card_dealer.rb
+++ NapakalakiRuby/lib/card_dealer.rb
@@ -154,89 +154,89 @@ module Napakalaki
 		def init_monster_card_deck
 			# Bicéfalo
 			p_bicefalo = Prize.new(1, 1)
-			bc_bicefalo = BadConsequence.newLevelSpecificTreasures("Te faltan manos para tanta cabeza. Pierdes 3 niveles y tus tesoros visibles de las manos.", 
+			bc_bicefalo = SpecificBadConsequence.new("Te faltan manos para tanta cabeza. Pierdes 3 niveles y tus tesoros visibles de las manos.", 
 				3, [TreasureKind::ONEHAND, TreasureKind::ONEHAND, TreasureKind::BOTHHANDS], [])
 			bicefalo = Monster.new_normal_monster("Bicefalo", 20, bc_bicefalo, p_bicefalo)
 			@unused_monsters.push(bicefalo)
 
 			# 3 Byakhees de bonanza
 			p_byakhees = Prize.new(2, 1)
-			bc_byakhees = BadConsequence.newLevelSpecificTreasures("Pierdes tu armadura visible y otra oculta.", 
+			bc_byakhees = SpecificBadConsequence.new("Pierdes tu armadura visible y otra oculta.", 
 				0, [TreasureKind::ARMOR], [TreasureKind::ARMOR])
 			byakhees = Monster.new_normal_monster("3 Byakhees de bonanza", 8, bc_byakhees, p_byakhees)
 			@unused_monsters.push(byakhees)
 
 			# Los hondos
-			bc_hondos = BadConsequence.newDeath("Estos monstruos resultan bastante superficiales y te aburren mortalmente. Estas muerto.")
+			bc_hondos = DeathBadConsequence.new("Estos monstruos resultan bastante superficiales y te aburren mortalmente. Estas muerto.")
 			p_hondos = Prize.new(2, 1)
 			hondos = Monster.new_normal_monster("Los hondos", 8, bc_hondos, p_hondos)
 			@unused_monsters.push(hondos)
 
 			# Chibithulhu
-			bc_chibi = BadConsequence.newLevelSpecificTreasures("Embobados con el lindo primigenio te descartas de tu casco visible.",
+			bc_chibi = SpecificBadConsequence.new("Embobados con el lindo primigenio te descartas de tu casco visible.",
 				0, [TreasureKind::HELMET], [])
 			p_chibi = Prize.new(1, 1)
 			chibi = Monster.new_normal_monster("Chibithulhu", 2, bc_chibi, p_chibi)
 			@unused_monsters.push(chibi)
 
 			# El sopor de Dunwich
-			bc_dunwich = BadConsequence.newLevelSpecificTreasures("El primordial bostezo contagioso. Pierdes tu calzado visible.", 
+			bc_dunwich = SpecificBadConsequence.new("El primordial bostezo contagioso. Pierdes tu calzado visible.", 
 				0, [TreasureKind::SHOES], [])
 			p_dunwich = Prize.new(1, 1)
 			dunwich = Monster.new_normal_monster("El sopor de Dunwich", 2, bc_dunwich, p_dunwich)
 			@unused_monsters.push(dunwich)
 
 			# Ángeles de la noche ibicenca
-			bc_angeles = BadConsequence.newLevelSpecificTreasures("Te atrapan para llevarte de fiesta y te dejan caer en mitad del vuelo. Pierdes una mano visible y otra oculta.", 
+			bc_angeles = SpecificBadConsequence.new("Te atrapan para llevarte de fiesta y te dejan caer en mitad del vuelo. Pierdes una mano visible y otra oculta.", 
 				0, [TreasureKind::ONEHAND], [TreasureKind::ONEHAND])
 			p_angeles = Prize.new(4, 1)
 			angeles = Monster.new_normal_monster("Angeles de la noche ibicenca", 14, bc_angeles, p_angeles)
 			@unused_monsters.push(angeles)
 
 			# El gorrón en el umbral
-			bc_gorron = BadConsequence.newLevelNumberOfTreasures("Pierdes todos tus tesoros visibles.", 
+			bc_gorron = NumericBadConsequence.new("Pierdes todos tus tesoros visibles.", 
 				0, BadConsequence.MAXTREASURES, 0)
 			p_gorron = Prize.new(3, 1)
 			gorron = Monster.new_normal_monster("El gorron en el umbral", 10, bc_gorron, p_gorron)
 			@unused_monsters.push(gorron)
 
 			# H.P. Munchcraft
-			bc_munchcraft = BadConsequence.newLevelSpecificTreasures("Pierdes la armadura visible.", 
+			bc_munchcraft = SpecificBadConsequence.new("Pierdes la armadura visible.", 
 				0, [TreasureKind::ARMOR], [])
 			p_munchcraft = Prize.new(2, 1)
 			munchcraft = Monster.new_normal_monster("H.P. Munchcraft", 6, bc_munchcraft, p_munchcraft)
 			@unused_monsters.push(munchcraft)
 
 			# Bichgooth
-			bc_bichgooth = BadConsequence.newLevelSpecificTreasures("Sientes bichos bajo la ropa. Descarta la armadura visible.", 
+			bc_bichgooth = SpecificBadConsequence.new("Sientes bichos bajo la ropa. Descarta la armadura visible.", 
 				0, [TreasureKind::ARMOR], [])
 			p_bichgooth = Prize.new(1, 1)
 			bichgooth = Monster.new_normal_monster("Bichgooth", 2, bc_bichgooth, p_bichgooth)
 			@unused_monsters.push(bichgooth)
 
 			# El rey de rosa
-			bc_reyrosa = BadConsequence.newLevelNumberOfTreasures("Pierdes 5 niveles y 3 tesoros visibles.", 
+			bc_reyrosa = NumericBadConsequence.new("Pierdes 5 niveles y 3 tesoros visibles.", 
 				5, 3, 0)
 			p_reyrosa = Prize.new(4, 2)
 			reyrosa = Monster.new_normal_monster("El rey de rosa", 13, bc_reyrosa, p_reyrosa)
 			@unused_monsters.push(reyrosa)
 
 			# La que redacta en las tinieblas
-			bc_tinieblas = BadConsequence.newLevelNumberOfTreasures("Toses los pulmones y pierdes 2 niveles.", 
+			bc_tinieblas = NumericBadConsequence.new("Toses los pulmones y pierdes 2 niveles.", 
 				2, 0, 0)
 			p_tinieblas = Prize.new(1, 1)
 			tinieblas = Monster.new_normal_monster("La que redacta en las tinieblas", 2, bc_tinieblas, p_tinieblas)
 			@unused_monsters.push(tinieblas)
 
 			# Semillas Cthulhu
-			bc_semillas = BadConsequence.newLevelNumberOfTreasures("Pierdes 2 niveles y 2 tesoros ocultos.",
+			bc_semillas = NumericBadConsequence.new("Pierdes 2 niveles y 2 tesoros ocultos.",
 				2, 0, 2)
 			p_semillas = Prize.new(2, 1)
 			semillas_cthulhu = Monster.new_normal_monster("Semillas Cthulhu", 4, bc_semillas, p_semillas)
 			@unused_monsters.push(semillas_cthulhu)
 
 			# Dameargo
-			bc_dameargo = BadConsequence.newLevelSpecificTreasures("Te intentas escaquear. Pierdes una mano visible.",
+			bc_dameargo = SpecificBadConsequence.new("Te intentas escaquear. Pierdes una mano visible.",
 				0, [TreasureKind::ONEHAND], [])
 			p_dameargo = Prize.new(2, 1)
 			dameargo = Monster.new_normal_monster("Dameargo", 1, bc_dameargo, p_dameargo)
